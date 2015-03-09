af04c0d1a009f1a562b3871e1ee812f9e9feb932
sasaki <sasaki@whizz-tech.co.jp>
2015-03-09 16:53:39 +0900

    AWSで動かせるように、設定

--- helloboot/src/main/java/com/example/App.java
+++ helloboot/src/main/java/com/example/App.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @EnableAutoConfiguration
-public class App {
+public class App extends SpringBootServletInitializer {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(App.class);
     }
 
     @RequestMapping("/")
     public String home() {
-        return "Hello, Boot!";
+        return "Hello, Boot2!";
     }
 }
