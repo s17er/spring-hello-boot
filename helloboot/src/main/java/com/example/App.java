4fc318b883f062e58d1adcebd1db0e5ddd5678c4
sasaki <sasaki@whizz-tech.co.jp>
2015-03-09 11:58:54 +0900

    Hello, Bootアプリの実装とpomの追加

--- helloboot/src/main/java/com/example/App.java
+++ helloboot/src/main/java/com/example/App.java
@@ -1,13 +1,23 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@RestController
+@EnableAutoConfiguration
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello, Boot!";
     }
 }
