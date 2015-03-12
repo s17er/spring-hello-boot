48bae1bf45d494fb944c8c6def8b1592dbdb6e6b
sasaki <sasaki@whizz-tech.co.jp>
2015-03-12 16:19:55 +0900

    登録処理実装

--- helloboot/src/main/java/com/example/web/ItemController.java
+++ helloboot/src/main/java/com/example/web/ItemController.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import io.undertow.attribute.RequestMethodAttribute;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +27,22 @@ public class ItemController {
 	@Autowired
 	ItemService itemService;
 	
+	/**
+	 * 画面のフォーム値とマッピングするフォームクラスの初期化
+	 * RequestMappingアノテーションがついているメソッドがコールされる前に実行され、
+	 * このメソッドが呼ばれた結果が、Model#addAttribute()により、モデルクラスにセットされる
+	 * @return
+	 */
+	@ModelAttribute
+	ItemForm setUpForm() {
+		return new ItemForm();
+	}
+	
+	/**
+	 * ToDo一覧画面を返す
+	 * @param model ブラウザに返す画面で参照するエンティティを受け渡すオブジェクト
+	 * @return 一覧画面のテンプレートパス
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	String list(Model model) {
 		// DBからエンティティを取得する
@@ -31,5 +51,35 @@ public class ItemController {
 		model.addAttribute("items", items);
 		// テンプレートのファイルパスを返す
 		return "items/list";
+	}
+	
+	/**
+	 * 登録画面を返す
+	 * 
+	 * @param form フォームクラス
+	 * @return 登録画面のテンプレートパス
+	 */
+	@RequestMapping(value = "add", params = "form",  method = RequestMethod.GET) 
+	String addForm(ItemForm form) {
+		return "items/add";
+	}
+
+	/**
+	 * 登録画面を返す
+	 * 
+	 * @param form 画面のフォームに入力された値がマッピングされたフォームクラス
+	 * @param result フォームのバリデーションを実行した結果が格納される（第一引数に@Validatedアノテーションが付いている場合）
+	 * @return 登録画面のテンプレートパス
+	 */
+	@RequestMapping(value = "add", method = RequestMethod.POST) 
+	String add(@Validated ItemForm form, BindingResult result) {
+		// バリデーションエラーの場合は追加画面に戻る（フォームクラスを渡すことにより、入力値は保たれる）
+		if(result.hasErrors()) {
+			return addForm(form);
+		}
+		Item item = new Item();
+		BeanUtils.copyProperties(form, item);
+		this.itemService.create(item);
+		return "redirect:/items";
 	}
 }
